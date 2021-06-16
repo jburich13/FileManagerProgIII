c8ee4f00fa06347652936732a28c88920f7d472f
Juan Burich <burichojrs@gmail.com>
2021-06-16 20:32:48 -0300

    Agregando crear archivos, solo layout

---
+++ CrearArchivos.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Windows.Forms;
+
+namespace FileManagerProgIII
+{
+    public partial class CrearArchivos : Form
+    {
+        public CrearArchivos()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void crearArchivos()
+        {
+            string path =InputDireccion.Text;
+
+
+        }
+        
+    }
+}
